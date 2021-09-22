d68ff48151289349220636855db42f64f396aeaa
hypernova1 <hypemova@gmail.com>
2021-09-22 10:56:50 +0900

    feat: Builder 학습

--- flutter_app_bar/lib/main.dart
+++ flutter_app_bar/lib/main.dart
@@ -12,63 +12,123 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red
-      ),
-      home: MyPage(),
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const MyPage(),
     );
   }
 }
 
 class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Appbar icon menu'),
-        centerTitle: true,
-        elevation: 0,
-        actions: [ // AppBar 오른쪽에 배치되는 버튼들
-          IconButton(
-            icon: const Icon(Icons.shopping_cart),
-            onPressed: () {
-              print('shopping cart');
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              print('search');
-            },
-          ),
-        ],
-      ),
-      drawer: Drawer( // leading 위치에 생성됨
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
+        appBar: AppBar(
+          title: const Text('Appbar icon menu'),
+          centerTitle: true,
+          elevation: 0,
+          actions: [
+            // AppBar 오른쪽에 배치되는 버튼들
+            IconButton(
+              icon: const Icon(Icons.shopping_cart),
+              onPressed: () {
+                print('shopping cart');
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                print('search');
+              },
+            ),
+          ],
+        ),
+        drawer: Drawer(
+          // leading 위치에 생성됨
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              UserAccountsDrawerHeader(
                 currentAccountPicture: const CircleAvatar(
                   backgroundImage: AssetImage('assets/a.jpg'),
                   backgroundColor: Colors.white,
                 ),
-              accountEmail: Text('hypemova@gmail.com'),
-              accountName: Text('Sam'),
-              onDetailsPressed: () {
+                accountEmail: const Text('hypemova@gmail.com'),
+                accountName: const Text('Sam'),
+                otherAccountsPictures: const [
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/a.jpg'),
+                    backgroundColor: Colors.white,
+                  ),
+                  // CircleAvatar(
+                  //   backgroundImage: AssetImage('assets/a.jpg'),
+                  //   backgroundColor: Colors.white,
+                  // )
+                ],
+                onDetailsPressed: () {
                   print('onpress');
-              },
-              decoration: BoxDecoration(
-                color: Colors.red[200],
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40)
+                },
+                decoration: BoxDecoration(
+                  color: Colors.red[200],
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(40),
+                      bottomRight: Radius.circular(40)),
                 ),
               ),
-            )
-          ],
+              ListTile(
+                leading: Icon(
+                  Icons.home,
+                  color: Colors.grey[850],
+                ),
+                title: const Text('home'),
+                onTap: () {
+                  print('home');
+                },
+                trailing: Icon(Icons.add),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.settings,
+                  color: Colors.grey[850],
+                ),
+                title: const Text('Setting'),
+                onTap: () {
+                  print('setting');
+                },
+                trailing: Icon(Icons.add),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.question_answer,
+                  color: Colors.grey[850],
+                ),
+                title: const Text('Q & A'),
+                onTap: () {
+                  print('qna');
+                },
+                trailing: const Icon(Icons.add),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+        body: Builder(
+          builder: (BuildContext ctx) { // 기존의 context 를 무시하고 새로운 context 를 생성한다.
+            return Center(
+              child: FlatButton(
+                child: const Text(
+                  'data',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.red,
+                onPressed: () {
+                  Scaffold.of(ctx).showSnackBar(const SnackBar(
+                    content: Text('Hello'),
+                  ));
+                },
+              ),
+            );
+          },
+        ));
   }
-
 }
