0a51cc8c7aba7d6a23eef8646f1b12e9e23b39a2
hypernova1 <hypemova@gmail.com>
2021-09-23 12:40:48 +0900

    feat: dice 페이지 렌더링 완료

--- flutter_login/lib/dice.dart
+++ flutter_login/lib/dice.dart
@@ -8,6 +8,41 @@ class Dice extends StatefulWidget {
 class _DiceState extends State<Dice> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      appBar: AppBar(
+        title: Text('Dice Game'),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(child: Image.asset('images/dice1.png')),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(child: Image.asset('images/dice2.png')),
+                ],
+              ),
+            ),
+            SizedBox(height: 60,),
+            ButtonTheme(
+                minWidth: 100,
+                height: 60,
+                child: RaisedButton(
+                  onPressed: () {},
+                  color: Colors.orangeAccent,
+                  child: Icon(Icons.play_arrow, color: Colors.white, size: 50,),
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
